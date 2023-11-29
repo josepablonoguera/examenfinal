8bf6dac303dc69e75b1186af1dd29f01b95276b5
josepablonoguera <josepablonoguera@gmail.com>
2023-11-28 20:29:45 -0600

    Update Ejercicio1.java

--- src/ejercicio1/Ejercicio1.java
+++ src/ejercicio1/Ejercicio1.java
@@ -9,25 +9,41 @@ package ejercicio1;
  * @author pablonoguera
  */
 public class Ejercicio1 {
-
+ 
+    
     public static void calcularFibonacci() {
         System.out.println("");
         System.out.println("Ejercicio 1");
-        System.out.println("Escribe una función recursiva en Java para calcular el n-ésimo término de la secuencia de Fibonacci.");
-        System.out.println("La sucesión de Fibonacci es una secuencia de números en la que cada número es la suma de los dos anteriores, comenzando con 0 y 1. La secuencia comienza así:\n"
-                + "\n"
-                + "[0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ....]\n"
-                + "\n"
-                + "En términos matemáticos, la sucesión de Fibonacci (F(n)) para un índice (n) se define de la siguiente manera:\n"
-                + "\n"
-                + "[F(n) = F(n-1) + F(n-2)]\n"
-                + "\n"
-                + "con las condiciones iniciales:\n"
-                + "\n"
-                + "[F(0) = 0, y F(1) = 1]\n"
-                + "\n"
-                + "Esto significa que cada número en la secuencia es la suma de los dos números anteriores.\nLa secuencia de Fibonacci tiene muchas aplicaciones en matemáticas y ciencias de la computación.\n");
-// Implementa la lógica del ejercicio 1 aquí
+     /* Ejercicio: Búsqueda Binaria Recursiva
+
+Implementa una función en Java que realice una búsqueda binaria en un arreglo ordenado.
+La función debe aceptar tres parámetros: el arreglo en el que se realizará la búsqueda, 
+el elemento que se desea encontrar y los índices de inicio y fin que definen la porción actual 
+del arreglo en la que se está buscando. 
+La función debe devolver el índice del elemento si se encuentra en el arreglo y -1 si no está presente.
+
+A continuación, se proporciona un ejemplo de declaración de la función:
+
+
+public static int busquedaBinaria(int[] arreglo, int elemento, int inicio, int fin) {
+    // Tu implementación aquí
+}
+
+
+Para realizar la búsqueda binaria, sigue estos pasos:
+
+1. Si el índice de inicio es mayor que el índice de fin, el elemento no está presente en el arreglo. 
+Devuelve -1.
+
+2. Calcula el índice medio del rango actual (suma el índice de inicio al índice de fin y divide por 2).
+
+3. Compara el elemento a buscar con el valor en el índice medio.
+    - Si son iguales, devuelve el índice medio.
+    - Si el elemento es menor que el valor medio, realiza una llamada recursiva en la mitad izquierda del arreglo (ajustando el índice de fin).
+    - Si el elemento es mayor que el valor medio, realiza una llamada recursiva en la mitad derecha del arreglo (ajustando el índice de inicio).
+
+Implementa el programa principal que demuestre el uso de esta función, proporcionando un arreglo ordenado y un elemento para buscar. Imprime el resultado de la búsqueda, indicando si el elemento se encuentra en el arreglo y en qué índice. */
+    // Tu implementación aquí    
     }
 
 }
